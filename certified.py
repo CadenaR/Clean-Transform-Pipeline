d330924a63dc821288c9809e45b06c617073a8d8
Rodrigo Cadena <cadena-25@hotmail.com>
2022-02-18 06:24:59 +0000

    Certified layer finished

---
+++ certified.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+
+
+def certify_data(raw_data_path, refined_path):
+    spark = SparkSession.builder.getOrCreate()
+
+    print("[Certified] Loading Data")
+    df = spark.read.csv(raw_data_path, header=True, inferSchema=True)
+
+    print("[Certified] Saving data in long format")
+    df.write.mode("overwrite").option("header", True).csv(refined_path + "all_data_long_format")
+
+    print("[Certified] Saving data in wide format")
+    df = df.groupBy('categoria').pivot('estado').sum('precio').orderBy('categoria').na.fill(0)
+
+    df.write.mode("overwrite").option("header", True).csv(refined_path + "all_data_wide_format")
+
+
+if __name__ == "__main__":
+    print("[Certified] Start")
+    refined_data_path = "./data/refined/all_data_refined"
+    certified_path = "./data/certified/"
+    certify_data(refined_data_path, certified_path)
+    print("[Certified] End")
